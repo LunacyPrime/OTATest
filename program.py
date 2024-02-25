baa967e721f0c7eea989710d7af43fd3e40b7018
LunacyPrime <110311192+LunacyPrime@users.noreply.github.com>
2024-02-25 16:09:26 +0100

    Update program.py

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 
-firmware_url = "https://github.com/LunacyPrime/OTATest/new/main/"
+firmware_url = "https://raw.githubusercontent.com/LunacyPrime/OTATest/new/main/"
 
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "program.py")
 ota_updater.download_and_install_update_if_available()
