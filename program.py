8623512ed95c5888f31f2cdd3b5b2f072249e193
LunacyPrime <110311192+LunacyPrime@users.noreply.github.com>
2024-02-25 16:14:21 +0100

    Update program.py

--- program.py
+++ program.py
@@ -5,7 +5,3 @@ firmware_url = "https://raw.githubusercontent.com/LunacyPrime/OTATest/new/main/"
 
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "program.py")
 ota_updater.download_and_install_update_if_available()
-
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test.py")
-
-ota_updater.download_and_install_update_if_available()
